522844fbfc481ad875aff37f4028d2fb876366e1
bayamasa <mapi0213mgt@gmail.com>
2022-10-28 20:53:21 +0900

    rename と コンストラクタ

--- includes/vector.hpp
+++ includes/vector.hpp
@@ -1,11 +1,13 @@
 #ifndef VECTOR_HPP
 # define VECTOR_HPP
 
-#include <exception>
+#include <stdexcept>
 #include <memory>
 #include "type_traits.hpp"
 #include "iterator.hpp"
 #include "reverse_iterator.hpp"
+#include <deque>
+#include <limits>
 
 namespace ft
 {
@@ -28,17 +30,18 @@ class vector
         typedef ft::reverse_iterator<iterator> reverse_iterator;
         typedef ft::reverse_iterator<const_iterator> const_reverse_iterator;
         
-        explicit vector(const Allocator& a = Allocator()) : alloc(a){};
+        explicit vector(const Allocator& a = Allocator()) : __alloc_(a){};
         
-        explicit vector(size_type n, const T& value = T(),
-                const Allocator& a = Allocator()) : alloc(a)
+        explicit vector(size_type __n, const_reference __value = T(),
+                const Allocator& __a = Allocator()) : __alloc_(__a)
         {
-            resize(n, value);
+            __vallocate(__n);            
+            __last_ = __uninitialized_fill_n(__first_, __n, __value);
         }
         template <class InputIter>
         vector(InputIter first, InputIter last,
                 const Allocator& a = Allocator(),
-                typename ft::enable_if<!ft::is_integral<InputIter>::value>::type* = NULL) : alloc(a)
+                typename ft::enable_if<!ft::is_integral<InputIter>::value>::type* = NULL) : __alloc_(a)
         {
             reserve(std::distance(first, last));
             for (InputIter i = first; i != last ; ++i)
@@ -47,40 +50,43 @@ class vector
             }
         };
         
-        vector (const vector & x);
+        vector(const vector & x);
         ~vector(){};
         vector & operator =(const vector & x);
         
-        iterator begin() { return first; }
-        const_iterator begin() const { return first; }
-        iterator end() { return last; }
-        const_iterator end() const { return last; }
+        iterator begin() { return __first_; }
+        const_iterator begin() const { return __first_; }
+        iterator end() { return __last_; }
+        const_iterator end() const { return __last_; }
         
-        reverse_iterator rbegin() { return reverse_iterator(last); }
-        const_reverse_iterator rbegin() const { return reverse_iterator(last); }
-        reverse_iterator rend() { return reverse_iterator(first); }
-        const_reverse_iterator rend() const { return reverse_iterator(first); }
+        reverse_iterator rbegin() { return reverse_iterator(__last_); }
+        const_reverse_iterator rbegin() const { return reverse_iterator(__last_); }
+        reverse_iterator rend() { return reverse_iterator(__first_); }
+        const_reverse_iterator rend() const { return reverse_iterator(__first_); }
         
         size_type size() const {
             return end() - begin();
         }
-        size_type max_size() const;
+        size_type max_size() const{
+            return std::min<size_type>(__alloc_.max_size(),
+                                 std::numeric_limits<difference_type>::max());
+        }
                     
         void resize(size_type sz, value_type c = value_type()) {
             if (sz < size()) {
                 size_type diff = size() - sz;
                 destroy_until(rbegin() + diff);
-                last = first + sz;
+                __last_ = __first_ + sz;
             } else if ( sz > size()) {
                 reserve(sz);
-                for (; last != reserved_last; ++last) {
-                    construct(last, c);
+                for (; __last_ != __end_of_storage_; ++__last_) {
+                    construct(__last_, c);
                 }
             }
         }
         
         size_type capacity() const {
-            return reserved_last - first;
+            return __end_of_storage_ - __first_;
         }
         
         bool empty() const {
@@ -91,66 +97,67 @@ class vector
             if (sz <= capacity())
                 return ;
             // 動的メモリの確保
-            pointer ptr = allocate(sz);
+            pointer ptr = NULL;
+            __vallocate(sz);
             
             // 古いストレージ情報を保存
-            pointer old_first = first;
-            pointer old_last = last;
+            pointer old_first = __first_;
+            pointer old_last = __last_;
             size_type old_capacity = capacity(); 
             
             // 新しいストレージに差し替え
-            first = ptr;
-            last = first;
-            reserved_last = first + sz;
+            __first_ = ptr;
+            __last_ = __first_;
+            __end_of_storage_ = __first_ + sz;
             
             // 古いストレージから新しいストレージに要素を差し替え
-            for (pointer old_iter = old_first; old_iter != old_last; ++old_iter, ++last) {
-                construct(last, std::move(*old_iter));
+            for (pointer old_iter = old_first; old_iter != old_last; ++old_iter, ++__last_) {
+                construct(__last_, std::move(*old_iter));
             }
             for (reverse_iterator riter = reverse_iterator(old_last), rend = reverse_iterator(old_first);
                     riter != rend; ++riter) {
                 destroy(&*riter);            
             }
               // 古いストレージの破棄
-            traits::deallocate(alloc, old_first, old_capacity);
+            __alloc_traits::deallocate(__alloc_, old_first, old_capacity);
         }
         
         reference operator [](size_type i) {
-            return first[i];
+            return __first_[i];
         }
         const_reference operator [](size_type i) const {
-            return first[i];
+            return __first_[i];
         }
         
         reference at(size_type i) {
             if (i >= size())
                 throw std::out_of_range("index is out of range.");
-            return first[i];
+            return __first_[i];
         }
         
         const_reference at(size_type i) const {
             if (i >= size())
                 throw std::out_of_range("index is out of range.");
-            return first[i];
+            return __first_[i];
         }
         
         pointer data()
-        { return first; }
+        { return __first_; }
 
         const_pointer data() const
-        { return first; }
+        { return __first_; }
         
         reference front()
-        { return first; }
+        { return __first_; }
 
         const_reference front() const
-        { return first; }
+        { return __first_; }
 
         reference back()
-        { return last - 1; }
+        { return __last_ - 1; }
 
         const_reference back() const
-        { return last - 1; }
+        { return __last_ - 1; }
         
         template <class InputIterator>
         void assign(InputIterator first, InputIterator last); 
@@ -172,8 +179,8 @@ class vector
                 reserve( c ) ;
             }
             
-            construct(last, value);
-            ++last;
+            construct(__last_, value);
+            ++__last_;
         }
         
         void pop_back();
@@ -200,39 +207,62 @@ class vector
         allocator_type get_allocator() const;
         
         void construct(pointer ptr) {
-            traits::construct(alloc, ptr);
+            __alloc_traits::construct(__alloc_, ptr);
         }
         void construct(pointer ptr, const_reference value) {
-            traits::construct(alloc, ptr, value);
-        }
-        void construct(pointer ptr, value_type & value) {
-            traits::construct(alloc, ptr, std::move(value));
+            __alloc_traits::construct(__alloc_, ptr, value);
         }
         
     private: 
-        // 先頭の要素へのポインター
-        pointer first;
-        // 最後の要素の1つ前方のポインター
-        pointer last;
-        // 確保したストレージの終端
-        pointer reserved_last;
-        // アロケーターの値
-        allocator_type alloc;
+        pointer __first_;
+        pointer __last_;
+        pointer __end_of_storage_;
+        allocator_type __alloc_;
         
-        typedef std::allocator_traits<allocator_type> traits;
+        typedef std::allocator_traits<allocator_type> __alloc_traits;
         
-        pointer allocate(size_type n) {
-            return traits::allocate(alloc, n);
+        // Called by constructors to check initial size.
+        size_type __check_init_len(size_type __n, const allocator_type & __a) {
+            if (__n > max_size(__a))
+                throw std::length_error("cannot create ft::vector larger than max_size()");
+            return __n;
+        }
+        
+
+        
+        template <class _ForwardIterator, class _Size, class _T>
+        _ForwardIterator
+        __uninitialized_fill_n(_ForwardIterator __first, _Size __n, const _T & __x)
+        {
+            _ForwardIterator __cur = __first;
+            try {
+                for (; __n > 0; --__n, (void) ++__cur) {
+                    __alloc_.construct(__cur, __x);
+                }
+                return __cur;
+            } catch(...) {
+                for (; __cur != __first; (void) ++__first) 
+                    __alloc_.destroy(__first);
+                throw;
+            }
+        }
+        
+        void __vallocate(size_type __n) {
+            if (__n > max_size())
+                throw std::length_error("cannot create ft::vector larger than max_size()");
+            __first_ = __alloc_.allocate(__n);
+            __last_ = __first_;
+            __end_of_storage_ = __first_ + __n;
         }
         void deallocate() {
-            traits::deallocate(alloc, first, capacity());
+            __alloc_traits::deallocate(__alloc_, __first_, capacity());
         };
 
         void destroy(pointer ptr) {
-            traits::destroy(alloc, ptr);
+            __alloc_traits::destroy(__alloc_, ptr);
         }
         void destroy_until(reverse_iterator rend) {
-            for (reverse_iterator riter = rbegin(); riter != rend; ++riter, --last) {
+            for (reverse_iterator riter = rbegin(); riter != rend; ++riter, --__last_) {
                 destroy(&*riter);
             }
         }
